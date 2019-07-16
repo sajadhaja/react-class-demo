44c92417f793ac9740e2979aa19a4ba17ffac3fa
sajadhaja <sajadhaja90@gmail.com>
2019-07-16 20:51:22 +0200

    1 file modified

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import Row from './Row';
-import { ThemeContext, LocaleContext } from './context'
 
 export default class Greeting extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Tom',
-      surName: 'Mathew',
-      width: window.innerWidth
+      name: 'Tom'
     }
     this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleSurNameChange = this.handleSurNameChange.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-
   }
   handleNameChange(e) {
     this.setState({
@@ -22,63 +16,16 @@ export default class Greeting extends React.Component {
     });
   }
 
-  handleSurNameChange(e) {
-    this.setState({
-      surName: e.target.value
-    });
-  }
-
-  componentDidMount() {
-    document.title = this.state.name + ' ' + this.state.surName;
-    window.addEventListener('resize',this.handleResize);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.name != this.state.name) {
-      document.title = this.state.name + ' ' + this.state.surName;
-    }
-  }
-
-  componentWillMount() {
-    console.log("@@@Unmount");
-    window.removeEventListener('resize',this.handleResize);
-  }
-
-  handleResize() {
-    this.setState({
-      width: window.innerWidth
-    })
-  }
-
   render() {
     return (
-      <ThemeContext>
-        {theme => (
-          <section className={theme}>
+          <section>
             <Row label="Name">
               <input
                 value={this.state.name}
                 onChange={this.handleNameChange} />
             </Row>
-            <Row label="Surname">
-              <input
-                value={this.state.surName}
-                onChange={this.handleSurNameChange} />
-            </Row>
-            <LocaleContext>
-              {locale => (
-                <Row label="Language">
-                  {locale}
-                </Row>
-              )}
-            </LocaleContext>
-            <Row label="width">
-              {this.state.width}
-            </Row>
+           
           </section>
-        )}
-      </ThemeContext>
-
     );
   }
 }
